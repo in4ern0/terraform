b282e4c482b00d60db930ad2060b57a8fc15ccf9
Ilkin <ilkin.mammadovv@gmail.com>
2023-05-30 01:31:27 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -5,5 +5,25 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = ">=3.3.0"
+    }
+    radom = {
+      source  = "hashicorp/random"
+      version = ">= 1.0.0"
+    }
+    azure = {
+      source  = "hashicorp/azurerm"
+      version = ">=1.0.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">=2.0.0 "
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">=1.0.0"
+    }
   }
 }
